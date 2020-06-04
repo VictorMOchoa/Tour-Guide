6278306f196f689466abd4468c21a615bb371248
Victor Ochoa <ochoa.m.victor@gmail.com>
2020-06-04 09:19:42 -0700

    Gradle setup

---
+++ app/src/main/java/com/eatanapple/tourguide/MainActivity.java
@@ -0,0 +1,20 @@
+package com.eatanapple.tourguide;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+//        viewPager.setAdapter();
+    }
+}
